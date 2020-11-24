675c4e12a306c4c746561b4caebbb8cac1b9c541
andriy.pentsak <andriy.pentsak@vitech.com.ua>
2020-11-24 22:47:33 +0200

    added bash as template

--- containter_host.tf
+++ containter_host.tf
@@ -1,6 +1,14 @@
-data "template_file" "init" {
+data "template_file" "config" {
   count = length(var.hosts)
   template = file("${path.module}/scripts/config.yml")
+  vars = {
+    container_name = element(split("-",element(var.hosts, count.index)),0)
+  }
+}
+
+data "template_file" "exporter_bash" {
+  count = length(var.hosts)
+  template = file("${path.module}/scripts/container_exporter.sh")
   vars = {
     container_name = element(split("-",element(var.hosts, count.index)),0)
   }
@@ -22,12 +30,12 @@ resource "null_resource" "container_exporter" {
   }
 
   provisioner "file" {
-    source      = "${path.module}/scripts/container_exporter.sh"
+    content     =  data.template_file.exporter_bash[count.index].rendered
     destination = "/home/${var.user}/container_exporter.sh"
   }
 
   provisioner "file" {
-    content      = data.template_file.init[count.index].rendered
+    content      = data.template_file.config[count.index].rendered
     destination = "/home/${var.user}/config.yml"
   }
 
--- scripts/container_exporter.sh
+++ scripts/container_exporter.sh
@@ -1,22 +1,9 @@
-host=$1
 mkdir -p jmx
 mv config.yml ./jmx
 docker rm -fv jmx_exporter
-docker volume rm jmx_exporter
-docker volume create --driver local --name jmx_exporter --opt type=none --opt device=`pwd`/jmx --opt o=uid=root,gid=root --opt o=bind
+docker volume rm jmx_config_volume
+docker volume create --driver local --name jmx_config_volume --opt type=none --opt device=`pwd`/jmx --opt o=uid=root,gid=root --opt o=bind
 docker run -d -p 10001:5556 \
---name jmx_exporter \
--v jmx_exporter:/opt/bitnami/jmx-exporter/example_configs \
+--name jmx_exporter --link ${container_name} \
+-v jmx_config_volume:/opt/bitnami/jmx-exporter/example_configs \
 bitnami/jmx-exporter:latest 5556 example_configs/config.yml
-
-#docker run -d --name=cadvisor_exporter --device=/dev/kmsg -p 9102:8080 \
-#-v /:/rootfs:ro -v /var/run:/var/run:ro -v /sys:/sys:ro \
-#-v /var/lib/docker/:/var/lib/docker:ro -v /dev/disk/:/dev/disk:ro \
-#gcr.io/cadvisor/cadvisor:v0.38.4
-#docker stop zookeeper || echo "Zookeeper doesn't exist"
-#docker run -d --name node_exporter -p 9101:9100 bitnami/node-exporter
-#docker run -d --name merge_exporter \
-#-e MERGER_PORT=8888 \
-#-e MERGER_URLS="http://$host:9101/metrics http://$host:9102/metrics http://$host:9103" \
-#-p 10001:8888 quay.io/rebuy/exporter-merger
-#docker start zookeeper || echo "Zookeeper doesn't exist"
